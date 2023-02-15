f714729be32a7f66b636cc39ec64545d590b901e
favourcoinitiative <thefavourcoinitiative@gmail.com>
2023-02-15 08:25:04 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
 import React from 'react';
-import './App.css';
-import Homepage from './components/homepage';
-import Header from './components/header';
-import Nowplayingpage from "./components/nowplayingpage";
-import {Routes, Route} from 'react-router-dom';
-import Sidebar from './components/sidebar'
-
 
 function App(){
   return(
-    <div className = 'appbdy'>
-     
-     <Header/>
-
-      <div className = 'app-body'>
-      <Sidebar/>
-      <Routes>
-      <Route path ='/' element = {<Homepage/>}/>
-      <Route path ='/nowplaying' element = {< Nowplayingpage/>}/>
-       
-     </Routes>
-     </div>
-     
-    
+    <div>
+      Hello
     </div>
   );
 }
